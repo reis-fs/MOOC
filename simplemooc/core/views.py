700224128b306b344b0aa626b787dcff2a53d97a
Felipe Reis <felipe.s.reis@live.com>
2017-05-24 03:29:06 -0300

    Criação do Projeto, criado o app "core", adição das páginas "home" e "contato", criação do banco de dados e criação do app courses.

---
+++ simplemooc/core/views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+
