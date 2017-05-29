b0db150841c4400a7b479bca6dc6a67f5c871377
Felipe Reis <felipe.s.reis@live.com>
2017-05-29 04:09:31 -0300

    Acrescentado o campo about na tabela.

--- simplemooc/courses/models.py
+++ simplemooc/courses/models.py
@@ -11,7 +11,8 @@ class CourseManager(models.Model):
 class Course(models.Model):
     name = models.CharField('Nome', max_length=100)
     slug = models.SlugField('Atalho')
-    description = models.TextField('Descrição', blank=True)
+    description = models.TextField('Descrição Simples', blank=True)
+    about = models.TextField('Sobre o Curso', blank=True)
     start_date = models.DateField('Data de Início', null=True, blank=True)
     image = models.ImageField(upload_to='courses/images', verbose_name='Imagem', null=True, blank=True)
     created_at = models.DateTimeField('Criado em', auto_now_add=True)
