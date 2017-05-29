21fb71f67878742e9d54772f53a0a9ae56aa9083
Felipe Reis <felipe.s.reis@live.com>
2017-05-29 02:18:03 -0300

    Editado a linguagem para pt-br em Settings, personalização do model admin e teste de CRUD com o admin.

--- simplemooc/courses/admin.py
+++ simplemooc/courses/admin.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Course
 
-# Register your models here.
+
+class CourseAdmin(admin.ModelAdmin):
+
+    list_display = ['name', 'slug', 'start_date', 'created_at']
+    search_fields = ['nome', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Course, CourseAdmin)
