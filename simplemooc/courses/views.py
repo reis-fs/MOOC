29127dd7d08df42e4c3105212d3b78fd642d8251
felipe <felipe.s.reis@live.com>
2017-06-12 20:41:04 -0300

    Criação e estilização do formulário da página Detalhes.

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 from django.shortcuts import render, get_object_or_404
 from .models import Course
+from .forms import ContactCourse
 
 
 def cursos(request):
@@ -25,8 +26,15 @@ def details(request, pk):
 
 def details(request, slug):
     course = get_object_or_404(Course, slug=slug)
-    context = {
-        'course': course
-    }
+    context = {}
+    if request.method == 'POST':
+        form = ContactCourse(request.POST)
+        if form.is_valid():
+            context['is_valid'] = True
+            form = ContactCourse()
+    else:
+        form = ContactCourse()
+    context['course'] = course
+    context['form'] = form
     template_name = 'courses/details.html'
     return render(request, template_name, context)
